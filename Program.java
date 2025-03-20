c5f293e152016eace53b5b89841b4fe263090dda
Bhaskar Das <dassbhaskar2002@gmail.com>
2025-03-20 20:56:16 +0530

    Entire Program Updated

--- Program.java
+++ Program.java
@@ -1,14 +1,19 @@
-// import java.util.*;
+// Program to find the Maximum Element in an Array
+import java.util.*;
 public class Program {
+	public static int FindMax(int arr[], int size) {
+		int max=0;
+		for(int i=0;i<size;i++) {
+			if(arr[i]>max)
+				max=arr[i];
+		}
+		return max;
+	}
 	public static void main(String args[]) {
-		int a=10,b=20;
-		System.out.println("a = "+a);
-		System.out.println("b = "+b);
-		int temp=a;
-		a=b;
-		b=temp;
-		System.out.println("------------------");
-		System.out.println("a = "+a);
-		System.out.println("b = "+b);
+		Scanner sc=new Scanner(System.in);
+		int length=sc.nextInt();
+		int arr[]=new int[length];
+		System.out.println(FindMax(arr, length));
+		sc.close();
 	}
 }
